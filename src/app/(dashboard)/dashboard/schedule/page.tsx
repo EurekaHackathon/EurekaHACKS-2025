ecfccfd5967e7169b1530897aaf870ea3f9a2086
Aaron <aaronye1001@gmail.com>
2025-01-29 17:41:00 -0500

    Add coming soon to schedule

--- src/app/(dashboard)/dashboard/schedule/page.tsx
+++ src/app/(dashboard)/dashboard/schedule/page.tsx
@@ -1,9 +1,9 @@
+import ComingSoon from "@/components/ComingSoon";
+
 export default function ApplicationPage() {
     return (
-        <div>
-            <h1>
-                sdfsdfj
-            </h1>
+        <div className="flex justify-center items-center h-screen">
+            <ComingSoon/>
         </div>
-    )
+    );
 }
---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,8 @@
+export default function ComingSoon() {
+    return (
+        <div className="text-gray-700">
+            <h1 className="text-5xl font-bold text-center">Coming soon</h1>
+            <h2 className="pt-4 text-2xl font-medium">The schedule will be released closer to the event date</h2>
+        </div>
+    )
+}
