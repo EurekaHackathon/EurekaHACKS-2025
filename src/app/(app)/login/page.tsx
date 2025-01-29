08b66fd2f4fa83ac2cf30d7072ec0f587c7d76a9
Jake Comay <jake.comay@gmail.com>
2025-01-29 15:01:22 -0500

    removed stupid admin page, fixed pages not taking full screen height

--- src/app/(app)/login/page.tsx
+++ src/app/(app)/login/page.tsx
@@ -6,6 +6,7 @@ import { validateSessionToken } from "@/lib/sessions";
 export default async function SignUpPage() {
     const cookieStore = await cookies();
     const sessionToken = cookieStore.get("session");
+    
     if (sessionToken) {
         const sessionValidationResult = await validateSessionToken(sessionToken.value);
         if (sessionValidationResult.session && sessionValidationResult.user) {
@@ -14,7 +15,7 @@ export default async function SignUpPage() {
     }
 
     return (
-        <div className="bg-secondary-200 flex items-center justify-center py-32">
+        <div className="bg-secondary-200 flex items-center justify-center py-32 flex-grow">
             <LoginForm/>
         </div>
     );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,6 @@ import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
 import { db } from "@/lib/database";
 import { createDBUser, CreateDBUserRow, updateDBUserPassword } from "@/lib/sqlc/auth_sql";
 import { hash } from "@node-rs/argon2";
-import { cookies } from "next/headers";
-import { jwtVerify, SignJWT } from "jose";
 
 export async function hashPassword(password: string): Promise<string> {
     return await hash(password, {
@@ -61,46 +59,3 @@ export async function generateEmailVerificationToken(): Promise<string> {
     crypto.getRandomValues(bytes);
     return encodeBase32LowerCaseNoPadding(bytes);
 }
-
-const JWTsignature = new TextEncoder().encode(process.env.ADMIN_SECRET);
-const adminSessionCookie = "admin-session";
-
-export async function loginAdmin(password: string): Promise<boolean> {
-    if (password != process.env.ADMIN_PASSWORD) {
-        return false;
-    }
-
-    let token = await new SignJWT()
-        .setProtectedHeader({ alg: "HS256" })
-        .setIssuedAt()
-        .setExpirationTime("4 week from now")
-        .sign(JWTsignature);
-
-    (await cookies()).set(adminSessionCookie, token);
-
-    return true;
-}
-
-export async function isAdmin(): Promise<boolean> {
-    let token = (await cookies()).get(adminSessionCookie)?.value;
-
-    if (!token) {
-        return false;
-    }
-
-    try {
-        await jwtVerify(token, JWTsignature, {
-            algorithms: ["HS256"],
-        });
-
-        return true;
-    } catch {
-        return false;
-    }
-}
-
-export async function signoutAdmin() {
-    (await cookies()).delete(adminSessionCookie);
-}
-
-
