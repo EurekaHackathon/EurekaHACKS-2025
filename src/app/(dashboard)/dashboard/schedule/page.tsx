beaf510ec89cb6c47257a485628ed6be00d83a02
Aaron <aaronye1001@gmail.com>
2025-02-26 22:11:21 -0500

    Add qr code page

--- src/app/(dashboard)/dashboard/schedule/page.tsx
+++ src/app/(dashboard)/dashboard/schedule/page.tsx
@@ -3,7 +3,7 @@ import ComingSoon from "@/components/ComingSoon";
 export default function ApplicationPage() {
     return (
         <div className="flex px-12 justify-center items-center h-screen">
-            <ComingSoon/>
+            <ComingSoon text="The schedule will be released closer to the event date."/>
         </div>
     );
 }
--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,9 +1,9 @@
 // TODO: Make responsive on mobile
-export default function ComingSoon() {
+export default function ComingSoon({ text }: { text: string }) {
     return (
         <div className="text-gray-700">
             <h1 className="text-5xl font-bold text-center">Coming soon</h1>
-            <h2 className="pt-4 text-2xl font-medium">The schedule will be released closer to the event date</h2>
+            <h2 className="pt-4 text-2xl font-medium">{text}</h2>
         </div>
-    )
+    );
 }
