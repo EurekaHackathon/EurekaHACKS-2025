f9874cdec6421192a0397e0a29456e92ce9436ec
Aaron <aaronye1001@gmail.com>
2025-02-27 00:26:36 -0500

    Add skeleton loading screen to dashboard home

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,108 +1,11 @@
-import Link from "next/link";
-import { Icon } from "@iconify/react";
-import { CountdownTimer } from "@/components/CountdownTimer";
-import { DeadlineCountdown } from "@/components/DeadlineCountdown";
-import { cookies } from "next/headers";
-import { authorizeSession } from "@/lib/sessions";
-import { redirect } from "next/navigation";
-import { getApplicationStatus } from "@/lib/sqlc/application_sql";
-import { db } from "@/lib/database";
+import DashboardSkeleton from "@/components/ui/DashboardSkeleton";
+import { Suspense } from "react";
+import DashboardHome from "@/components/ui/DashboardHome";
 
 export default async function Dashboard() {
-    const cookiesStore = await cookies();
-    const sessionToken = cookiesStore.get("session");
-    const user = await authorizeSession(sessionToken?.value);
-    if (!user) {
-        redirect("/login");
-    }
-    const applicationStatus = await getApplicationStatus(db, {
-        userId: user.id
-    });
-
-    const firstName = user?.firstName ? user?.firstName : "Hacker";
-
     return (
-        <div className="self-center flex flex-col items-stretch py-[10vh] px-8">
-            <h1 className="text-5xl font-bold text-gray-700">
-                Welcome,&nbsp;
-                <span className="block md:inline text-secondary-600">{firstName}</span>
-            </h1>
-            <div className="border border-gray-300 mt-12 rounded-lg bg-gray-50 py-8 px-12">
-                <div className="flex justify-between">
-                    <h2 className="text-gray-600 font-semibold text-2xl">Application status</h2>
-                    <Icon icon="fluent:form-multiple-48-filled" className="text-gray-500 text-4xl"/>
-                </div>
-                {(applicationStatus?.status === "unsubmitted" || !applicationStatus) &&
-                    <>
-                      <h1 className="text-secondary-600 font-bold text-5xl pt-6">Not submitted</h1>
-                      <p className="text-gray-600 text-lg pt-2 pb-8 font-medium">
-                        You haven't started your application yet. Click the button below to start your application.
-                      </p>
-                      <Link href="/dashboard/application"
-                            className="bg-secondary-600 text-xl py-2 px-4 rounded-lg text-gray-100 font-medium hover:bg-[#815eeb] duration-200">
-                        Open application
-                      </Link>
-                    </>
-                }
-                {applicationStatus?.status === "submitted" &&
-                    <>
-                      <h1 className="text-secondary-600 font-bold text-5xl pt-6">Submitted</h1>
-                      <p className="text-gray-600 text-lg pt-2 pb-8 font-medium">
-                        Your application has been submitted. We will review your application and get back to you soon.
-                      </p>
-                    </>
-                }
-            </div>
-            <div className="flex 2xl:flex-row flex-col gap-8 mt-8">
-                <div className="border border-gray-300 rounded-lg bg-gray-50 py-8 px-12 flex-1 overflow-hidden">
-                    <div className="flex justify-between">
-                        <h2 className="text-gray-500 font-semibold text-2xl">Time until application deadline</h2>
-                        <Icon icon="fluent:clock-12-filled" className="text-gray-500 text-4xl"/>
-                    </div>
-                    <DeadlineCountdown/>
-                    <p className="text-gray-600 text-lg pt-2 pb-8 font-medium">
-                        Application due on March 22, 2025 at 11:59 PM EST.
-                    </p>
-                </div>
-                <div className="border border-gray-300 rounded-lg bg-gray-50 py-8 px-12 flex-1 overflow-hidden">
-                    <div className="flex justify-between">
-                        <h2 className="text-gray-500 font-semibold text-2xl">Time until Hackathon</h2>
-                        <Icon icon="fluent:hourglass-half-16-regular" className="text-gray-500 text-4xl"/>
-                    </div>
-                    <CountdownTimer/>
-                </div>
-            </div>
-            <div className="flex flex-col md:flex-row gap-8 mt-8">
-                <div className="border border-gray-300 rounded-lg bg-gray-50 py-8 px-12 flex-1 overflow-hidden">
-                    <div className="flex justify-between">
-                        <h2 className="text-gray-600 font-semibold text-2xl">Discord server</h2>
-                        <Icon icon="ic:baseline-discord" className="text-gray-500 text-4xl"/>
-                    </div>
-                    <h1 className="text-secondary-600 font-bold text-5xl pt-6">Our Discord server</h1>
-                    <p className="text-gray-600 text-lg pt-2 pb-8 font-medium">
-                        Join our Discord to connect with hackers and receive important announcements.
-                    </p>
-                    <a href="https://discord.gg/ApEmE7g7GB" target="_blank" rel="noopener noreferrer"
-                       aria-disabled="true" tabIndex={-1}
-                       className="pointer-events-none bg-secondary-300 text-xl py-2 px-4 rounded-lg text-gray-100 font-medium hover:bg-[#815eeb] duration-200">
-                        Coming soon
-                    </a>
-                </div>
-                <div className="border border-gray-300 rounded-lg bg-gray-50 py-8 px-12 flex-1 overflow-hidden">
-                    <div className="flex justify-between">
-                        <h2 className="text-gray-600 font-semibold text-2xl">Hacker package</h2>
-                        <Icon icon="fluent:book-information-20-filled" className="text-gray-500 text-4xl"/>
-                    </div>
-                    <h1 className="text-secondary-600 font-bold text-5xl pt-6">View hacker package</h1>
-                    <p className="text-gray-600 text-lg pt-2 pb-8 font-medium">
-                        View the hacker package, complete with event info and schedules for EurekaHACKS 2025.
-                    </p>
-                    <Link href="/dashboard" aria-disabled="true" tabIndex={-1}
-                          className="pointer-events-none bg-secondary-300 text-xl py-2 px-4 rounded-lg text-gray-100 font-medium hover:bg-[#815eeb] duration-200">
-                        Coming soon
-                    </Link>
-                </div>
-            </div>
-        </div>
+        <Suspense fallback={<DashboardSkeleton/>}>
+            <DashboardHome/>
+        </Suspense>
     );
 }
