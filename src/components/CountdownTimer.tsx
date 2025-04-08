a45a87bfe152a111cf1d0594169bafbf7c4b00a0
Aaron <aaronye1001@gmail.com>
2025-04-07 21:16:33 -0400

    Update dashboard countdown

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -7,7 +7,7 @@ const calculateTimeLeft = (eventDate: number) => {
     const difference = eventDate - now;
 
     if (difference <= 0) {
-        return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+        return {days: 0, hours: 0, minutes: 0, seconds: 0};
     }
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
@@ -15,7 +15,7 @@ const calculateTimeLeft = (eventDate: number) => {
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    return { days, hours, minutes, seconds };
+    return {days, hours, minutes, seconds};
 };
 
 export function CountdownTimer() {
@@ -32,25 +32,26 @@ export function CountdownTimer() {
 
     return (
         <div className="flex gap-2 text-secondary-600 font-bold text-3xl md:text-5xl pt-6 flex-wrap">
-            <div>
-                <h1 className="">{String(timeLeft.days).padStart(2, "0")}</h1>
-                <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Days</h2>
-            </div>
-            <h1>:</h1>
-            <div>
-                <h1 className="">{String(timeLeft.hours).padStart(2, "0")}</h1>
-                <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Hours</h2>
-            </div>
-            <h1>:</h1>
-            <div>
-                <h1 className="">{String(timeLeft.minutes).padStart(2, "0")}</h1>
-                <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Minutes</h2>
-            </div>
-            <h1>:</h1>
-            <div>
-                <h1 className="">{String(timeLeft.seconds).padStart(2, "0")}</h1>
-                <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Seconds</h2>
-            </div>
+            <h1>Event is over</h1>
+            {/*<div>*/}
+            {/*    <h1 className="">{String(timeLeft.days).padStart(2, "0")}</h1>*/}
+            {/*    <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Days</h2>*/}
+            {/*</div>*/}
+            {/*<h1>:</h1>*/}
+            {/*<div>*/}
+            {/*    <h1 className="">{String(timeLeft.hours).padStart(2, "0")}</h1>*/}
+            {/*    <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Hours</h2>*/}
+            {/*</div>*/}
+            {/*<h1>:</h1>*/}
+            {/*<div>*/}
+            {/*    <h1 className="">{String(timeLeft.minutes).padStart(2, "0")}</h1>*/}
+            {/*    <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Minutes</h2>*/}
+            {/*</div>*/}
+            {/*<h1>:</h1>*/}
+            {/*<div>*/}
+            {/*    <h1 className="">{String(timeLeft.seconds).padStart(2, "0")}</h1>*/}
+            {/*    <h2 className="text-center text-xs md:text-base lg:text-lg font-semibold">Seconds</h2>*/}
+            {/*</div>*/}
         </div>
     );
 }
